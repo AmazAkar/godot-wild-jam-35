33a07a80fe9debd678866eaa42a2c65e58ba0029
AmazAkar <67762131+AmazAkar@users.noreply.github.com>
2021-07-14 11:30:04 +0100

    Update wind.gd

--- levels/Scripts/wind.gd
+++ levels/Scripts/wind.gd
@@ -1,13 +1,6 @@
 extends Sprite
 
 onready var AP = $AnimationPlayer
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "te	
-
-
-#
-
 
 func _on_TimerWnd_timeout():
 	AP.play("coremtr")
