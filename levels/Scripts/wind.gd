4630af5d26b0354f073551835ee3c41ae95a6a0a
jagoldrick <jagoldrick@outlook.com>
2021-07-14 15:06:33 +0100

    further changes to wind ui

--- levels/Scripts/wind.gd
+++ levels/Scripts/wind.gd
@@ -2,5 +2,18 @@ extends Sprite
 
 onready var AP = $AnimationPlayer
 
+func _ready():
+	visible = false
+
+
+	
+
 func _on_TimerWnd_timeout():
 	AP.play("coremtr")
+	
+	visible = true
+	
+
+func _on_TimerPhys_timeout():
+	AP.stop()
+	visible = false
