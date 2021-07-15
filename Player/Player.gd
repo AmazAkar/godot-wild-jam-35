1c24b300a6375bb1300af09b67cca0c2d8dc54d5
Amaz Akar <akarsh.choudhary2004@gmail.com>
2021-07-15 10:38:18 +0100

    tidied up Player.gd code

--- Player/Player.gd
+++ Player/Player.gd
@@ -9,15 +9,18 @@ const vel_force = 50
 const torque_force = 2000
 const torque_force2 = 10000
 
-const RL = 5
-
-var level3 = false;
 
 enum {
 	track1,
 	track2,
 }
 var track;
+enum {
+	default,
+	level2,
+	level3,
+}
+var level = default;
 
 onready var AP = $AnimationPlayer
 onready var sprite = $Player
@@ -27,58 +30,55 @@ onready var col_wheel = $Collision_Wheel
 onready var col_player = $Collision_Player
 onready var tween = $Tween
 var col_track2;
-	
-	
 
 func _ready():
 	if get_tree().get_current_scene().get_name() == "Level3":
 		set_physics_process(false)
 		remove_child(camera)
-		
+		level = level3
+	if get_tree().get_current_scene().get_name() == "Level2":
+		level = level2
 func _physics_process(delta):
-	if !level3:
-		if Input.is_action_pressed("move-left") and self.linear_velocity.x > -max_force:
-			self.linear_velocity.x -= vel_force
-			sprite.flip_h = true
-			#self.add_central_force(Vector2(-move_force.x,0))
-		if Input.is_action_pressed("move-right") and self.linear_velocity.x < max_force:
-			self.linear_velocity.x += vel_force
-			sprite.flip_h = false
-			#self.add_force(force_position, move_force)
+	if Input.is_action_pressed("move-left") and self.linear_velocity.x > -max_force:
+		self.linear_velocity.x -= vel_force
+		sprite.flip_h = true
+		#self.add_central_force(Vector2(-move_force.x,0))
+	if Input.is_action_pressed("move-right") and self.linear_velocity.x < max_force:
+		self.linear_velocity.x += vel_force
+		sprite.flip_h = false
+		#self.add_force(force_position, move_force)
+	if Input.is_action_pressed("move-right") or Input.is_action_pressed("move-left"):
+		AP.play("cycle")
+	else: 
+		AP.stop()
 
-		if get_tree().get_current_scene().get_name() != "Level2":
+	match level:
+		default:
 			if Input.is_action_just_pressed("balance-left"):
-				self.apply_torque_impulse(-torque_force)
+					self.apply_torque_impulse(-torque_force)
 
-		if get_tree().get_current_scene().get_name() != "Level2":
 			if Input.is_action_just_pressed("balance-right"):
 				self.apply_torque_impulse(torque_force)
-
-		if get_tree().get_current_scene().get_name() == "Level2":
+			
+		level2:
 			if Input.is_action_just_pressed("balance-left"):
 				self.apply_torque_impulse(-torque_force2)
-
-		if get_tree().get_current_scene().get_name() == "Level2":
+				
 			if Input.is_action_just_pressed("balance-right"):
 				self.apply_torque_impulse(torque_force2)
 
-		if Input.is_action_pressed("move-right") or Input.is_action_pressed("move-left"):
-			AP.play("cycle")
-		else: 
-			AP.stop()
-	else:
-		if Input.is_action_just_pressed("track-up") and track == track1:
-			self.apply_central_impulse(Vector2(0,-385))
-			yield(get_tree().create_timer(1),"timeout")
-			col_track2.disabled = false
-			track = track2
-			
-		if Input.is_action_just_pressed("track-down") and track == track2:	
-			col_track2.disabled = true
-			self.apply_central_impulse(Vector2(0,40))
-			yield(get_tree().create_timer(1),"timeout")
-			track = track1
-
+		level3:
+			if Input.is_action_just_pressed("track-up") and track == track1:
+				self.apply_central_impulse(Vector2(0,-385))
+				yield(get_tree().create_timer(1),"timeout")
+				col_track2.disabled = false
+				track = track2
+				
+			if Input.is_action_just_pressed("track-down") and track == track2:	
+				col_track2.disabled = true
+				self.apply_central_impulse(Vector2(0,40))
+				yield(get_tree().create_timer(1),"timeout")
+				track = track1
 
 func _on_Timer_timeout():
 	self.apply_torque_impulse(-torque_force)
